d90bcbe1bf249f9d3cb4c7e9b53461ce0389ef2f
Dejene Techane <dtechane6@gmail.com>
2024-05-24 12:09:55 -0400

    user profile updates buttons

--- app/static/js/form_editor.js
+++ app/static/js/form_editor.js
@@ -11,12 +11,28 @@
             input.classList.remove('read-only-input');
         });
 
-        // Show the save button and hide the edit button in the section
-        document.getElementById(`saveButton${capitalizeFirstLetter(section)}`).style.display = 'block';
-        document.getElementById(`editButton${capitalizeFirstLetter(section)}`).style.display = 'none';
+        // Show the save and cancel buttons, hide the edit button
+    document.getElementById(`saveButton${capitalizeFirstLetter(section)}`).style.display = 'block';
+    document.getElementById(`cancelButton${capitalizeFirstLetter(section)}`).style.display = 'block';
+    document.getElementById(`editButton${capitalizeFirstLetter(section)}`).style.display = 'none';
     }
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    function cancelForm(section) {
+        // Get the input fields within the specified section
+        const inputs = document.querySelectorAll(`#${section}Form input`);
+    
+        // Restore original values and make inputs read-only
+        inputs.forEach(input => {
+            input.setAttribute('readonly', 'readonly');
+            input.classList.add('read-only-input');
+        });
+    
+        // Hide the save and cancel buttons, show the edit button
+        document.getElementById(`saveButton${capitalizeFirstLetter(section)}`).style.display = 'none';
+        document.getElementById(`cancelButton${capitalizeFirstLetter(section)}`).style.display = 'none';
+        document.getElementById(`editButton${capitalizeFirstLetter(section)}`).style.display = 'block';
+    }
