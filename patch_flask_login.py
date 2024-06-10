a93ac2b2a8209ce26b0dcb6d5d44d3d55f79760a
Dejene Techane <dtechane6@gmail.com>
2024-06-10 01:56:39 -0400

    workflow pytest error updated

--- patch_flask_login.py
+++
@@ -1,8 +0,0 @@
-# patch_flask_login.py
-from werkzeug import parse_query_string
-
-def patched_url_decode(*args, **kwargs):
-    return parse_query_string(*args, **kwargs)
-
-from flask_login.utils import url_decode
-url_decode = patched_url_decode
